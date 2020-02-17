ed771d92db6a943f78a74f1244527a2c1fe09622
revzet77 <revzet.j@gmail.com>
2020-02-17 12:50:26 -0500

    added guidance on unit test script

--- Assets/_Scripts/testing.cs
+++ Assets/_Scripts/testing.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// the testing script is meant to cover all our User stories in the sprint backlog
+// current automated user stories:
+// 003: As a gamer, I want my game to run smoothly so it immerses me.
+// 007: As a gamer, I want to have interesting enemies that behave naturally so I am immersed in the game.
+// 011: As a gamer, I want to be able to see the status of my health and weaponry.
+// ALL unit tests must go with a user story
 public class testing : MonoBehaviour
 {
     // AI Manager tests
