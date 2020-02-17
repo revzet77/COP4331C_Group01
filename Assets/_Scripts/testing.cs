74100e1508426390c1cc8b84aeb18e4dcb1565ef
revzet77 <revzet.j@gmail.com>
2020-02-17 12:34:14 -0500

    added automated testing scene

---
+++ Assets/_Scripts/testing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testing : MonoBehaviour
+{
+    // AI Manager tests
+    // Mode Manager tests
+    // Movement Manager tests
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+}
