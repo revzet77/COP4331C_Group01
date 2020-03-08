d014a77b9baba523615b05506cc9989c133ba7e6
revzet77 <revzet.j@gmail.com>
2020-03-08 19:33:29 -0400

    Revert "added comments"
    This reverts commit 333975b8e1277e730258f322cad734b98db54736.

--- Assets/_Scripts/Tests/Testing.cs
+++ Assets/_Scripts/Tests/Testing.cs
@@ -63,7 +63,7 @@ public class testing
 
     }
     
-    // User Story 000 / 013 / 001
+    // User Story 000 / 013
     [Test]
     public void test_inputHandler(){
         InputHandler inp = new InputHandler();
@@ -71,7 +71,7 @@ public class testing
 
     }
     
-    // User Story 000 / 001
+    // User Story 00
     [Test]
     public void test_CameraController(){
         CameraController cam = new CameraController();
@@ -83,7 +83,7 @@ public class testing
 
     }
     
-    // User Story 000 
+    // User Story 00
     [Test]
     public void test_PlayerController(){
         PlayerController play = new PlayerController();
@@ -110,7 +110,7 @@ public class testing
     }
     
     // UI Manager test
-    // User Stories 005 / 008 / 011 / 010
+    // User Stories 005 / 008 / 011
     [Test]
     public void test_UIManager(){
         
