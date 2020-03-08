2dd1dd697958fd25167c2b9f5ee145cb99485ff5
revzet77 <revzet.j@gmail.com>
2020-03-08 16:40:01 -0400

    added basic AI nav

---
+++ Assets/_Scripts/MovementController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent meleeRange;
+    public UnityEngine.AI.NavMeshAgent midRange;
+    public UnityEngine.AI.NavMeshAgent longRange;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meleeRange.SetDestination(player.GetComponent<Transform>().position);
+        midRange.SetDestination(player.GetComponent<Transform>().position);
+        longRange.SetDestination(player.GetComponent<Transform>().position);
+    }
+}
