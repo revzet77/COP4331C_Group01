08841f07ae40c87a3d4586407d9f9194dfc01226
revzet77 <revzet.j@gmail.com>
2020-03-08 15:37:15 -0400

    added navmeshcomponents, edited AI script names

---
+++ Assets/_Scripts/ModeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// manages the mode for all current active enemies
+public class ModeController : MonoBehaviour
+{
+    enum AttackMode{
+        Offense = 0,
+        Defense = 1
+    }
+
+    public class enemyMode{
+
+        private int curMode;
+
+        public enemyMode(){
+            curMode = 1;
+        }
+
+        public enemyMode(int mode){
+            curMode = mode;
+        }
+        public int getMode(){
+            return curMode;
+        }
+    }
+
+}
